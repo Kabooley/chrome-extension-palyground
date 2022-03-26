f2eb8b030d10fd1ec43d80eab94e64473956dd09
Kabooley <35451559+Kabooley@users.noreply.github.com>
2022-03-26 18:29:25 +0900

    Fix: fixed Circulater.ts file typeerror and syntaxerror

--- src/utils/Circulater.ts
+++ src/utils/Circulater.ts
@@ -1,22 +1,25 @@
-export interface iCallbackOfCirculater {
-  <T>(): Promise<T>;
+// NOTE: iCallbackOfCirculater<T>とiConditionOfCirculater<T, K>
+// のGenericsのT型は共通であること
+export interface iCallbackOfCirculater<T> {
+  (): Promise<T>;
 }
-export interface iConditionOfCirculater {
-  <T>(operand: T): boolean;
+// GenericsK型にしているけど必ずboolean型にすること
+export interface iConditionOfCirculater<T> {
+  (operand: T): boolean;
 }
-export interface iClosureOfCirculater {
-  <T>(): Promise<T>;
+export interface iClosureOfCirculater<T> {
+  (): Promise<T>;
 }
 
 // HIGH ORDER FUNCTION
 //
 // 再利用性のある非同期関数の任意ループ処理ラッパー
-export const circulater = function (
-  callback: iCallbackOfCirculater,
-  condition: iConditionOfCirculater,
+export const circulater = function <T>(
+  callback: iCallbackOfCirculater<T>,
+  condition: iConditionOfCirculater<T>,
   until: number
-): iClosureOfCirculater {
-  return async function <T>() {
+): iClosureOfCirculater<T> {
+  return async function () {
     // 予めループの外にresult変数を置いて
     let result: T;
     for (let i = 0; i < until; i++) {
@@ -32,3 +35,53 @@ export const circulater = function (
     return result;
   };
 };
+
+// USAGE
+
+// // 実際に実行したい関数
+// const counter = async (times: number): Promise<boolean> => {
+//   return new Promise((resolve, reject) => {
+//     let timerId: number;
+//     let num: number = 0;
+//     timerId = setInterval(function () {
+//       console.log(`counter: ${num}`);
+//       if (num >= times) {
+//         clearInterval(timerId);
+//         const random_boolean = Math.random() < 0.7;
+//         resolve(random_boolean ? true : false);
+//       } else num++;
+//     }, 1000);
+//   });
+// };
+
+// // circulaterへ渡すcallback関数
+// //
+// // 完全にハードコーディング
+// //
+// // 実際に実行したい関数へ渡さなくてはならない引数はここで渡すこと
+// // 戻り値は任意であるが、condition関数のgenerics型と同じにすること
+// const cb: iCallbackOfCirculater<boolean> = async (): Promise<boolean> => {
+//   const n: boolean = await counter(7);
+//   console.log(`cb: ${n}`);
+//   return n;
+// };
+
+// // circulaterへ渡すconditon関数
+// //
+// // 完全にハードコーディング
+// //
+// // circulaterへ渡す引数callbackの戻り値の型と同じ型をgenericsとして渡すこと
+// const counterCondition: iConditionOfCirculater<iOp> = (
+//   operand: iOp
+// ): boolean => {
+//   console.log(`condition: ${operand ? true : false}`);
+//   return operand ? true : false;
+// };
+
+// const counterLoop = circulater<boolean>(cb, counterCondition, 3);
+
+// (async function () {
+//   const r = await counterLoop();
+//   console.log(`RESULT: ${r}`);
+// })();
+
